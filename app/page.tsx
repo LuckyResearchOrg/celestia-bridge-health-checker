3d71e021350b8e5edce593359dcc5626b96bc300
LuckyResearch Labs <lyvuha@gmail.com>
2025-03-30 17:12:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
       <>
         {/*<Head>*/}
-        {/*  <title>Celestia Bridge Node Health Checker</title>*/}
-        {/*  <meta property="og:title" content="Celestia Bridge Node Health Checker" />*/}
-        {/*  <meta property="og:description" content="Welcome to DTEAM's Celestia Bridge Health Checker, the ultimate tool for ensuring the reliability and security of your Celestia bridge node. Designed with the community in mind, this checker allows you to easily verify the health of your bridge nodes. Simply enter your IP, port, and authentication token to get started."/>*/}
+        {/*  <title>Celestia Bridge Node Status Checker</title>*/}
+        {/*  <meta property="og:title" content="Celestia Bridge Node Status Checker" />*/}
+        {/*  <meta property="og:description" content="Welcome to LuckyResearch's Celestia Bridge Status Checker – a simple and efficient tool for monitoring the status of your Celestia bridge node. Enter your IP, port, and authentication token to get started!"/>*/}
         {/*  <meta property="og:type" content="website" />*/}
-        {/*  <meta property="og:url" content="https://celestia-bridge-checker.dteam.tech" />*/}
+        {/*  <meta property="og:url" content="https://celestia-bridge-checker.luckyresearch.org" />*/}
         {/*  <meta property="og:image" content="https://raw.githubusercontent.com/DTEAMTECH/contributions/main/celestia/utils/celestia_bridge.png?d=3" />*/}
 
         {/*  <meta property="og:image:width" content="1200" />*/}
