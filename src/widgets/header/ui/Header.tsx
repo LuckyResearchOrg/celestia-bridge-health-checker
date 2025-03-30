91a6263466b58ba5568fbafdedf91100b131a709
LuckyResearch Labs <lyvuha@gmail.com>
2025-03-30 16:36:42 +0700

    Update Header.tsx

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -4,8 +4,8 @@ import styles from "./Header.module.scss"
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>Celestia Bridge Health Checker</h1>
-            <p>{"Welcome to DTEAM's Celestia Bridge Health Checker, the ultimate tool for ensuring the reliability and security of your Celestia bridge node. Designed with the community in mind, this checker allows you to easily verify the health of your bridge nodes. Simply enter your IP, port, and authentication token to get started."}
+            <h1>Celestia Bridge Status Checker</h1>
+            <p>{"Welcome to LuckyResearch's Celestia Bridge Status Checker – a simple and efficient tool for monitoring the status of your Celestia bridge node. Enter your IP, port, and authentication token to get started!"}
             </p>
         </header>
     );
